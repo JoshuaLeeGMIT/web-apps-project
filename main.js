316e9b195f54a667c03aae71052fe2c186a26fa7
Joshua Lee <lee.son.wai@gmail.com>
2021-01-05 00:07:33 +0000

    main: Create pool

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mysql = require('promise-mysql');
+
 const app = express();
+const pool = mysql.createPool({
+  connectionLimit: 16,
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'geography'
+})
 
 app.get('/', (req, res) => {
   res.send("Hello, world!");
